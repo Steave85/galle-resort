ef5d80e2e32603aa11c6067aa57a16c5793880d3
Steave85 <meewalaarachchi.net@gmail.com>
2020-10-01 21:50:24 +0530

    Changes added

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -55,7 +55,7 @@ export default function ContactUs() {
 
     function onEmailChange(e){
         const email = e.target.value;
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if(re.test(String(email).toLowerCase()))
             setState({...state, [e.target.name]: { isValid: true, error: '' }});
         else
